5502db02a61673134dc52641cfabba1b517bdf4a
Kelvin <kelvinmurithi2005@gmail.com>
2024-04-15 18:30:26 +0300

    Looping lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+let event = "birthday";
+
+function writeCards(names, event) {
+  const message = [];
+  for (let i = 0; i < names.length; i++) {
+    message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return message;
+}
+const message = writeCards(names, event);
+console.log(message);
+
+function countDown() {
+  let countDown = 10;
+  while (countDown >= 0) {
+    console.log(countDown--);
+  }
+}
